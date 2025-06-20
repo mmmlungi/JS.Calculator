864d30205a811e453640b0471c41ba63d78ba4fa
Mlungisi <mmmlungi@gmail.com>
2025-06-20 14:12:29 +0200

    Lesson :3 Removing 0 on the dispay

--- script.js
+++ script.js
@@ -7,6 +7,37 @@ let justCalculated = false;
 function appendToDisplay(value) {
     console.log('button pressed: value');
 
+    let currentValue = display.value;
+
+    if (justCalculated) {
+        // If we just calculated, clear the display
+        currentValue = '';
+        justCalculated = false;
+        return
+    }
+
+    // Append the value to the display
+    if (currentValue === '0') {
+        // If the display is currently 0, replace it with the new value
+        display.value = value;
+    }
+    else {
+        // Otherwise, append the new value
+        display.value += value;
+    }
+    console.log('Current display value: ', display.value);
+    // Alert the user about the value pressed
+    // reset justCalculated flag when a button is pressed
+
+    justCalculated = false;
+
+    console.log('justCalculated flag reset to false');
+    display.value = currentValue + value;
+
+    function cleardisplay() {
+    console.log('clear button was pressed');      
+    }
+
     alert('You pressed: ' + value);
     
 }
@@ -19,6 +50,15 @@ function cleardisplay() {
 
 function deleteLast() {
     console.log('Backspace button pressed');
+
+    let currentValue = display.value;
+
+    if (currentValue.length > 1 || currentValue === '0') {
+        // Remove the last character if there are more than one characters
+        display.value = currentValue.slice(0, -1);
+    }
+
+
 
     alert('backspace button was clicked');
     }
